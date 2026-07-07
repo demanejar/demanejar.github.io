5bab92553dee7d152090c8401cb37069406bc084
Nguyen Quang Huy <trannguyenhan01092000@gmail.com>
2026-07-07 23:10:04 +0700

    add some post and remove polyfill

--- assets/js/data/swcache.js
+++ assets/js/data/swcache.js
@@ -60,8 +60,7 @@ const allowedDomains = [
 
   'fonts.gstatic.com',
   'fonts.googleapis.com',
-  'cdn.jsdelivr.net',
-  'polyfill.io'
+  'cdn.jsdelivr.net'
 ];
 
 /* Requests that include the following path will be banned */
